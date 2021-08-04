c39b4f09f99f777b2ef2370694d7d42220347244
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-08-05 01:35:09 +0900

    내비게이션바 설정 및 부트스트랩, JQuery 설정 완료

---
+++ home/static/js/navbar.js
@@ -0,0 +1,30 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+window.onload = function() {
+    console.log(getCookie('account'));
+    if (getCookie('account') == '3') {
+        var dropdown = document.getElementById('navbarDropdownMenuLink');
+        dropdown.innerHTML = '내 정보';
+        dropdown.setAttribute('href', '');
+    }
+}
