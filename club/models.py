60d4ab2f4ba87dde07169ddc741f4036b0859bb1
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-08-06 00:41:08 +0900

    동아리 목록 반응형 작업 및 동아리명 추가 완료

--- club/models.py
+++ club/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.deletion import SET_DEFAULT
+from home.models import User
 
 class Club(models.Model):
     name = models.CharField(max_length=200)
@@ -7,8 +9,9 @@ class Club(models.Model):
     isofficial = models.BooleanField(default=False)
     member = models.IntegerField()
     create_date = models.DateTimeField()
-    club_master = models.CharField(max_length=20, default='')
+    club_master = models.ForeignKey(User, on_delete=SET_DEFAULT, default='')
     club_teacher = models.CharField(max_length=20, default='')
+    club_thumbnail = models.TextField(default='')
     
     def __str__(self):
         return self.name
