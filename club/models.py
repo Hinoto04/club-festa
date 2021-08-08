81c9020dc809e2e0641e74097e12a423f1e2133e
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-08-08 17:35:50 +0900

    동아리 모델 수정

--- club/models.py
+++ club/models.py
@@ -8,6 +8,7 @@ class Club(models.Model):
     category = models.CharField(max_length=20, default='기타')
     isofficial = models.BooleanField(default=False)
     member = models.IntegerField()
+    member_detail = models.TextField(default='')
     create_date = models.DateTimeField()
     club_master = models.ForeignKey(User, on_delete=SET_DEFAULT, default='')
     club_teacher = models.CharField(max_length=20, default='')
