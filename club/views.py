de2c3dc4940c37ee16e2aed7d0f6716715affa56
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-08-09 21:00:38 +0900

    동아리 목록 필터 기능 추가

--- club/views.py
+++ club/views.py
@@ -3,10 +3,26 @@ from django.shortcuts import render
 from .models import Club
 from django.core.paginator import Paginator
 
+category = {'korean':'국어',
+            'math':'수학',
+            'english':'영어',
+            'science':'과학',
+            'society':'사회',
+            'etc':'기타'}
+
 def index(request):
     """클럽 목록 출력"""
     page = request.GET.get('page', '1')
-    club_list = Club.objects.order_by('name')
+    official = request.GET.get('official', '2')
+    categoryeng = request.GET.get('category', 'all')
+    club_list = Club.objects.all()
+    if categoryeng != 'all':
+        club_list = club_list.filter(category=category[categoryeng])
+    if official == '0':
+        club_list = club_list.filter(isofficial=False)
+    elif official == '1':
+        club_list = club_list.filter(isofficial=True)
+    club_list = club_list.order_by('name')
     last = len(club_list)//9 if len(club_list) == 0 else len(club_list)//9+1
     
     paginator = Paginator(club_list, 9)
