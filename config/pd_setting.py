908d172edf69d56e7b721a475b259583b3fd992f
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-09-05 18:56:09 +0900

    기간설정파일 생성

---
+++ config/pd_setting.py
@@ -0,0 +1,31 @@
+import datetime
+
+"""
+각종 기간 설정용 파일
+"""
+
+#창체동아리 가입기간
+OFFI_CLUB_REGI_OPEN = datetime.date(
+    year = 2022,
+    month = 3,
+    day = 20
+)
+
+OFFI_CLUB_REGI_CLOSE = datetime.date(
+    year = 2022,
+    month = 3,
+    day = 31
+)
+
+#자율동아리 가입기간
+NOOF_CLUB_REGI_OPEN = datetime.date(
+    year = 2022,
+    month = 4,
+    day = 1
+)
+
+NOOF_CLUB_REGI_CLOSE = datetime.date(
+    year = 2022,
+    month = 4,
+    day = 7
+)
