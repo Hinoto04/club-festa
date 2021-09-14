64505feb20b4c8ed0ddfc33d12a99bc6f7e89e02
kyj5311 <69591650+kyj5311@users.noreply.github.com>
2021-09-15 01:24:33 +0900

    모델 추가

--- event/models.py
+++ event/models.py
@@ -4,6 +4,9 @@ from django.db.models.deletion import CASCADE
 from club.models import Club
 
 class Event(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(default='', max_length=100)
     host = models.ForeignKey(Club, on_delete=CASCADE)
     content = models.TextField()
+    start_date = models.DateTimeField(default=datetime(2021, 9, 12, 0, 0, 0, 0), auto_created=True)
+    end_date = models.DateTimeField(default=datetime(2021, 9, 12, 0, 0, 0, 0), auto_created=True)
+    place  = models.CharField(default='', max_length=100)
