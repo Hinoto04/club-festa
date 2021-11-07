c8ced37a4213fbf950c6e18b46b74f8675d3e18c
kyj5311 <69591650+kyj5311@users.noreply.github.com>
2021-11-08 01:53:30 +0900

    싹다 갈아업어 주세요~~

--- event/urls.py
+++ event/urls.py
@@ -7,5 +7,7 @@ app_name = 'event'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:event_month>/', views.index, name='index'),
+    path('detail/<int:event_id>/', views.detail, name='detail'),
     path('registration/', views.registration, name='registration'),
 ]
--- event/views.py
+++ event/views.py
@@ -5,9 +5,34 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Event
 from .forms import EventForm
 
-def index(request):
-    all_event = Event.objects.all()
-    return render(request, 'event/event_main.html', {'all_event':all_event})
+from datetime import date
+
+def index(request, event_month=None):
+    class day:
+        date = None
+        event = []
+        
+        def __init__(self, date):
+            date = date
+    
+    #대충 첫 날짜
+    #startdate = date.
+    month = []
+    #month에 주별로 day 추가
+    #for i in range(5):
+    #    week = []
+    #    for j in range(7):
+    #        week.append(day(date.))
+    events = Event.objects.filter(start_date__year=2021).filter(start_date__month=11)|Event.objects.filter(end_date__year=2021).filter(end_date__month=11)
+    #for event in events:
+    #    날짜 확인 후 해당 날짜에 event 추가
+    #    기간제인 경우 해당 기간의 모든 날짜에 event 추가
+    
+    context = {
+        'year_month': '2021-11',
+        'month': month
+    }
+    return render(request, 'event/event_main.html', context)
 
 def registration(request):
     if request.method == "POST":
@@ -18,3 +43,11 @@ def registration(request):
         else:
             print(event_form)
     return render(request,'event/event_reg.html',{'event_form':EventForm()})
+
+def detail(request, event_id):
+    try:
+        event = Event.objects.get(id=event_id)
+    except:
+        return render(request, 'error.html', {'text':['이벤트가 존재하지 않습니다.']})
+    else:
+        return render(request, 'event/event_detail.html', {'event': event})
