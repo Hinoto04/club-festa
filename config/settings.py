20c7bad86ed235b7601927e8dbd6287423ffbe50
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-12-16 22:41:54 +0900

    secret key 위치 변경

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-from config import pd_setting
+from config import pd_setting, secret_key
 from . import db_setting
 from . import mail_secret
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open(BASE_DIR / 'secretkey.txt', 'r').readline
+SECRET_KEY = secret_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
