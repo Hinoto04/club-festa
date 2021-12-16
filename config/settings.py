013354131b7a0d141ab61ca55c1eeb83cd7264d1
kyj5311 <69591650+kyj5311@users.noreply.github.com>
2021-12-17 00:50:14 +0900

    장소 표시 추가

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "3.35.218.227",
