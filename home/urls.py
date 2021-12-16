f8b6a2bae0904bd193b20f57dcc97283dc54e2db
Hinoto04 <61084127+Hinoto04@users.noreply.github.com>
2021-12-17 00:49:58 +0900

    회원가입 진짜 비활성화

--- home/urls.py
+++ home/urls.py
@@ -8,7 +8,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    #path('register/', views.register, name='register'),
     path('checkid/', views.checkmail, name='checkmail'),
     path('login/', auth_views.LoginView.as_view(template_name='home/home_login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
